4a16ae912cd78ca86397b4617a457ff435f88df8
NewRound <86642886+NewRound@users.noreply.github.com>
2023-09-25 19:53:42 +0900

    에니메이션 컨트롤러 제작

---
+++ Assets/Scripts/Controller/AnimationController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class AnimationController : MonoBehaviour
+{
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int IsHit = Animator.StringToHash("IsHit");
+
+    protected CharacterController controller;
+    protected Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += Move;
+
+    }
+
+    private void Move(Vector2 obj)
+    {
+        animator.SetBool(IsWalking, obj.magnitude > .5f);
+    }
+
+    private void Attacking()
+    {
+        animator.SetTrigger(Attack);
+    }
+
+    private void Hit()
+    {
+        animator.SetBool(IsHit, true);
+    }
+
+    private void InvincibilityEnd()
+    {
+        animator.SetBool(IsHit, false);
+    }
+}
